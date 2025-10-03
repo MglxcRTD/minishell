2008b7d1e65d5173ea0072935042cde6a3653fc7
Miguel <MiguelRoadToDam@gmail.com>
2025-10-03 16:54:07 +0200

    3ºDIA
    He avanzado un poco mas pero posiblemente me toque rehacer muchas cosas

--- minishell/src/main/Main.java
+++ minishell/src/main/Main.java
@@ -1,7 +1,9 @@
 package main;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.io.InputStreamReader;
+
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,67 +14,118 @@ import tokenizer.TLine;
 
 public class Main {
 
-	public static void ejecutarComandosForeground(String comando) {
+	public static void ejecutarComandosForeground(String lineacomando) {
+
+		if (lineacomando == null || lineacomando.trim().isEmpty()) {
+			System.err.println("Error: comando vacío o inválido.");
+			return;
+		}
 
 		try {
-			TLine linea = Tokenizer.tokenize(comando);
+
+			TLine linea = Tokenizer.tokenize(lineacomando);
+
+			if (linea == null || linea.getCommands() == null || linea.getCommands().isEmpty()) {
+				System.err.println("Error: comando vacío o inválido.");
+			}
+
+			for (TCommand comando : linea.getCommands()) {
+				List<String> argumentos = comando.getArgv();
+				ProcessBuilder pb = new ProcessBuilder(argumentos);
+				pb.inheritIO();
+				try {
+					Process p = pb.start();
+					try (BufferedReader salida = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+						String lineasalida;
+						while ((lineasalida = salida.readLine()) != null) {
+							System.out.println(lineasalida);
+						}
+					}
+
+					try (BufferedReader errores = new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
+						String lineaerrores;
+						while ((lineaerrores = errores.readLine()) != null) {
+							System.err.println(lineaerrores);
+						}
+					}
+
+					try {
+						int codigosalida = p.waitFor();
+						if (codigosalida != 0) {
+							System.err.println("El proceso ha terminado con error: " + codigosalida);
+						} else {
+							System.out.println("Proceso terminado correctamente, salida: " + codigosalida);
+						}
+
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						System.err.println(e.getMessage());
+					}
+
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					System.err.println("No se pudo imprimir el comando: " + String.join(" ", argumentos));
+				}
+			}
+
+		} catch (MissingFileException e) {
+			// TODO Auto-generated catch block
+			System.err.println(e.getMessage());
+		}
+
+	}
+
+	public static void ejecutarComandosBackground(String lineaentrada) {
+
+		if (lineaentrada == null || lineaentrada.trim().isEmpty()) {
+			System.err.println("Error: comando vacío o inválido.");
+			return;
+		}
+
+		try {
+
+			TLine linea = Tokenizer.tokenize(lineaentrada);
+
+			if (linea == null || linea.getCommands() == null || linea.getCommands().isEmpty()) {
+				System.err.println("Error: comando vacío o inválido.");
+			}
+
 			for (TCommand cmd : linea.getCommands()) {
 				List<String> argumentos = cmd.getArgv();
 				ProcessBuilder pb = new ProcessBuilder(argumentos);
 				pb.inheritIO();
 				try {
 					Process p = pb.start();
-					try {
-						int codigodesalida = p.waitFor();
-						System.out.println("Proceso finalizado con codigo: " + codigodesalida);
-
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+					try (BufferedReader salida = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+						String lineasalida;
+						while ((lineasalida = salida.readLine()) != null) {
+							System.out.println(lineasalida);
+						}
 					}
+
+					try (BufferedReader errores = new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
+						String lineaerrores;
+						while ((lineaerrores = errores.readLine()) != null) {
+							System.err.println(lineaerrores);
+						}
+					}
+
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
-					e.printStackTrace();
+					System.err.println("No se pudo imprimir el comando: " + String.join(" ", argumentos));
 				}
 			}
 
 		} catch (MissingFileException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.println(e.getMessage());
 		}
 
 	}
-	
-	public static void ejecutarComandosBackground(String comando) {
-		try {
-			TLine linea = Tokenizer.tokenize(comando);
-			for(TCommand cmd : linea.getCommands()) {
-				List<String>argumentos = cmd.getArgv();
-				ProcessBuilder pb = new ProcessBuilder(argumentos);
-				pb.inheritIO();
-				try {
-					Process p = pb.start();
-					
-					
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			
-			
-		} catch (MissingFileException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	
+
 	public static void ejecutarComandosConRedireccion(String comando) {
-		
+
 	}
-	
-	
 
 	public static String clasificarComandos(String comando) {
 		if (comando.contains("|")) {
@@ -98,6 +151,11 @@ public class Main {
 		System.out.println("Introduce el nombre que quieres que salga en la shell");
 
 		String nombre = sc.nextLine();
+
+		if (nombre.isEmpty()) {
+			nombre = "default";
+		}
+
 		String comando = "";
 		while (true) {
 			System.out.print("\u001B[32m" + nombre + "@>\u001B[0m ");
